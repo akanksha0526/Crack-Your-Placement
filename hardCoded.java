4139dace1b2db1d92317eb3a7366c3febc28ee79
Akanksha Tripathi <97952543+akanksha0526@users.noreply.github.com>
2024-07-17 23:43:46 +0530

    Create hardCoded.java

---
+++ hardCoded.java
@@ -0,0 +1,17 @@
+//Do-everything-yourself code
+class Solution {
+    public String intToRoman(int num) {
+        StringBuilder ans = new StringBuilder();
+
+        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String ths[] ={"","M","MM","MMM"};
+
+ans.append(ths[num / 1000]);
+ans.append(hrns[(num % 1000) / 100]);
+ans.append(tens[(num % 100) / 10]);
+ans.append(ones[num % 10]);
+return ans.toString();
+    }
+}
